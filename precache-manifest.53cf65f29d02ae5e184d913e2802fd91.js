f49f40f3cbf1cb4e5abb019e64c507173da84871
Yaron Lavi <yawikflame@gmail.com>
2020-07-10 17:14:14 -0700

    Updates

---
+++ precache-manifest.53cf65f29d02ae5e184d913e2802fd91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e587c3852494af881c228c5e0793f0",
+    "url": "/react-cash-machine/index.html"
+  },
+  {
+    "revision": "a7ffaaaac632fe604550",
+    "url": "/react-cash-machine/static/js/2.a97aea64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-cash-machine/static/js/2.a97aea64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9f9255e051d2f64428",
+    "url": "/react-cash-machine/static/js/main.e5870217.chunk.js"
+  },
+  {
+    "revision": "27c16dc0628688230659",
+    "url": "/react-cash-machine/static/js/runtime-main.587dd073.js"
+  }
+]);
