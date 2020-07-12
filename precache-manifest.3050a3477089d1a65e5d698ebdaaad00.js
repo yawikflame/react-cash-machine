b3f46e411eaec67ffa99fcd81a831a133c6f3e63
Yaron Lavi <yawikflame@gmail.com>
2020-07-11 23:46:47 -0700

    Updates

---
+++ precache-manifest.3050a3477089d1a65e5d698ebdaaad00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46465beb5ca072b7a9556a8322da2bf2",
+    "url": "/react-cash-machine/index.html"
+  },
+  {
+    "revision": "53ba093d5ab12cb75025",
+    "url": "/react-cash-machine/static/js/2.47108388.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-cash-machine/static/js/2.47108388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a9e11f4a4f34649263f",
+    "url": "/react-cash-machine/static/js/main.b004100c.chunk.js"
+  },
+  {
+    "revision": "27c16dc0628688230659",
+    "url": "/react-cash-machine/static/js/runtime-main.587dd073.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-cash-machine/precache-manifest.acae96f8b45b421b9595b2ecdce1f4db.js"
+  "/react-cash-machine/precache-manifest.3050a3477089d1a65e5d698ebdaaad00.js"
 );
 
 self.addEventListener('message', (event) => {
