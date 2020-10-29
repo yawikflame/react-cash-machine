22cf39177bf493c852e9d31ee948591d40c4eb2d
yawikflame <yawikflame@gmail.com>
2020-10-29 20:12:31 +0200

    added milligram

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
 import App from './components/App';
+import 'milligram';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware()));
